adec2ede0268baa8acf983ca26807b011acd51be
srinu-tippana <svas7246@gmail.com>
2022-11-06 23:54:34 +0530

    Initial version of SPRINGBOOT application for Uploading Deleting and Downloading  file using amazon S3

---
+++ src/main/java/com/enhance/netcracker/NetCrackerFileUploadApplication.java
@@ -0,0 +1,13 @@
+package com.enhance.netcracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetCrackerFileUploadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetCrackerFileUploadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/enhance/netcracker/NetCrackerFileUploadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enhance.netcracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetCrackerFileUploadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
